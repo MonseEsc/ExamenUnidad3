af12514626597d7b36bef0d7a48743f478f41f6a
Monserrat Escobar <escobarmonserrat454@gmail.com>
2021-04-05 10:42:00 -0500

    Segunda version Monserrat

--- examon3/views.py
+++ examon3/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse 
 # Create your views here.
+def index(request):
+	contenido = """
+
+	<html>
+	<head>
+		<style>
+			body{
+			margin: 9em;
+			text-align: center;
+			color: purple;
+			background-color: rgb(231,152,231);
+			}
+		</style>
+	</head>
+	<body>
+		<h1> ITI8MB</h1>
+		<h1>MONSERRAT ESCOBAR MARTINEZ</h1>
+		<h1>1318283177</h1>
+	</body>
+	</html>
+	"""
+	return HttpResponse(contenido)
